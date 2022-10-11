2f8338c43e1bc6e3e1f5d9d9bc178bd4000fa154
Teresa <luvmargaritas@gmail.com>
2022-10-10 22:52:20 -0500

    add basic files for server

---
+++ index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const { start } = require('./server');
+start();
---
+++ server.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const express = require( 'express' );
+const PORT = process.env.PORT || 3002;
+const app = express();
